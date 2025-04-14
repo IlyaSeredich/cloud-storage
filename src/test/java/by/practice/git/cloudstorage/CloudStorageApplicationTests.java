89aa8b796d3fd9b7ba1695d0725fff0646dda807
Ilya Seredich <seredichilya2010@gmail.com>
2025-04-15 02:46:16 +0300

    Initial commit: cloud-storage project setup

---
+++ src/test/java/by/practice/git/cloudstorage/CloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package by.practice.git.cloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
